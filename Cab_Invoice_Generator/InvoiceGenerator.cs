56fbe7a70723a3f2cda83902ee23779882b99f01
VSRUJANA <srujana.valavala@gmail.com>
2020-10-30 18:16:03 +0530

    [srujana] Add. Test cases TC 1.1, TC 1.2 added

---
+++ Cab_Invoice_Generator/InvoiceGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cab_Invoice_Generator
+{
+    public class InvoiceGenerator
+    {
+        public double CalculateFare(double distance, int time)
+        {
+            return 0;
+        }
+    }
+}
